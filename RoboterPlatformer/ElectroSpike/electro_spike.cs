2e1aa6992c6ab6a31d6a7827685b954756597b40
fzthan <116592841+fzthan@users.noreply.github.com>
2024-03-22 22:44:59 +0100

    deals now damage to player

--- RoboterPlatformer/ElectroSpike/electro_spike.cs
+++ RoboterPlatformer/ElectroSpike/electro_spike.cs
@@ -4,13 +4,19 @@ using System;
 
 public partial class electro_spike : Area3D
 {
+	CharacterBody3D playerNode;
+	float damage = 1;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		playerNode = GetNode<Player>("/root/Test/Player");
 	}
 
-	public void _on_body_entered(Node3D body){
-		GD.Print("Hello World!");
+	public void _on_body_entered(Player body){
+		if(body == playerNode){
+			body.Health -= damage;
+			GD.Print(body.Health);
+		}
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
