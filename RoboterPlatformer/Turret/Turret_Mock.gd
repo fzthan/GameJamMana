3d7e100bae8f91d71ad1916b6e9533f8bb273eea
DennisVenturini <118184773+DennisVenturini@users.noreply.github.com>
2024-03-23 16:47:47 +0100

    Turret improvements
    improved turrestsasdfasd

--- RoboterPlatformer/Turret/Turret_Mock.gd
+++ RoboterPlatformer/Turret/Turret_Mock.gd
@@ -5,6 +5,8 @@ var time_Elapsed = 0
 var bullet = load("res://Bullet/Bullet.tscn")
 var instance
 
+@export var turret_Range = 30.0
+
 @onready var turret = $RayCast3D
 
 # Called when the node enters the scene tree for the first time.
@@ -17,15 +19,14 @@ func _process(delta):
 	time_Elapsed += delta
 	
 	var direction = get_parent().get_node("Player").global_transform.origin
-	print(direction)
 	turret.look_at(direction, Vector3.UP)
 	
 	look_at(direction, Vector3.UP)
 	rotation.x = 0
 	rotation.z = 0
+	print(direction.distance_to(global_transform.origin))
 	
-	
-	if (time_Elapsed >= 2.00):
+	if (time_Elapsed >= 2.00 && direction.distance_to(global_transform.origin) <= turret_Range):
 		time_Elapsed = 0
 		
 		instance = bullet.instantiate()
