1efdcdd18f9cdb0eed1cfa2c7667b1f12e80d004
Jan Nothacker <git@jan-nothacker.de>
2024-03-29 23:29:46 +0100

    Format Game Controller Code

--- RoboterPlatformer/gameController/gameController.cs
+++ RoboterPlatformer/gameController/gameController.cs
@@ -5,70 +5,78 @@ public partial class gameController : Node
 {
 	[Export]
 	Player player;
-  [Signal]
-  public delegate void GamePausedEventHandler(bool isPaused);
-  [Signal]
-  public delegate void PlayerDeadEventHandler(bool playerDead);
-  [Signal]
-  public delegate void PlayerDiedEventHandler(int live);
-  [Export]
-  public Checkpoint activeCheckpoint;
-  private int live = 2;
-  private bool isPaused = false;
-  private bool playerDead = false;
+	[Signal]
+	public delegate void GamePausedEventHandler(bool isPaused);
+	[Signal]
+	public delegate void PlayerDeadEventHandler(bool playerDead);
+	[Signal]
+	public delegate void PlayerDiedEventHandler(int live);
+	[Export]
+	public Checkpoint activeCheckpoint;
+	private int live = 2;
+	private bool isPaused = false;
+	private bool playerDead = false;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		player.HealthChanged += OnPlayerHealthChanged;
-	activeCheckpoint.SetActive(true);
+		activeCheckpoint.SetActive(true);
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-	if(Input.IsActionJustPressed("ui_cancel")) {
-	  isPaused = !isPaused;
-	  EmitSignal(SignalName.GamePaused, isPaused);
-	  GetTree().Paused = isPaused;
-	  if(isPaused)
-		Input.MouseMode = Input.MouseModeEnum.Visible;
-	  else
-		Input.MouseMode = Input.MouseModeEnum.Captured;
-	}
-	}
-
-	private async void OnPlayerHealthChanged(float oldHealth, float Health){
-		if(Health <= 0 && live == 2) {
-	  await ToSignal(GetTree().CreateTimer(1), "timeout");
-	  EmitSignal(SignalName.PlayerDied, live);
-	  live--;
-	  switch(activeCheckpoint.SpawnLocation) {
-		case Checkpoint.SPAWN_LOCATIONS.LEFT:
-		player.ResetAndReposition(activeCheckpoint.GetNode<Node3D>("SpawnLocations/Left").GlobalPosition);
-		break;
-		case Checkpoint.SPAWN_LOCATIONS.RIGHT:
-		player.ResetAndReposition(activeCheckpoint.GetNode<Node3D>("SpawnLocations/Right").GlobalPosition);
-		break;
-		case Checkpoint.SPAWN_LOCATIONS.FRONT:
-		player.ResetAndReposition(activeCheckpoint.GetNode<Node3D>("SpawnLocations/Front").GlobalPosition);
-		break;
-	  }
-		}else if(Health <= 0 && live < 2){
-	  await ToSignal(GetTree().CreateTimer(1), "timeout");
-	  playerDead = true;
-	  EmitSignal(SignalName.PlayerDead, playerDead);
-	  EmitSignal(SignalName.PlayerDied, live);
-	  GetTree().Paused = true;
-	  Input.MouseMode = Input.MouseModeEnum.Visible;
-	}
+		if (Input.IsActionJustPressed("ui_cancel"))
+		{
+			isPaused = !isPaused;
+			EmitSignal(SignalName.GamePaused, isPaused);
+			GetTree().Paused = isPaused;
+			if (isPaused)
+				Input.MouseMode = Input.MouseModeEnum.Visible;
+			else
+				Input.MouseMode = Input.MouseModeEnum.Captured;
+		}
 	}
 
-  public void _OnPlayerRegister(Checkpoint checkpoint) {
-	activeCheckpoint.SetActive(false);
-	activeCheckpoint = checkpoint;
-  }
+	private async void OnPlayerHealthChanged(float oldHealth, float Health)
+	{
+		if (Health <= 0 && live == 2)
+		{
+			await ToSignal(GetTree().CreateTimer(1), "timeout");
+			EmitSignal(SignalName.PlayerDied, live);
+			live--;
+			switch (activeCheckpoint.SpawnLocation)
+			{
+				case Checkpoint.SPAWN_LOCATIONS.LEFT:
+					player.ResetAndReposition(activeCheckpoint.GetNode<Node3D>("SpawnLocations/Left").GlobalPosition);
+					break;
+				case Checkpoint.SPAWN_LOCATIONS.RIGHT:
+					player.ResetAndReposition(activeCheckpoint.GetNode<Node3D>("SpawnLocations/Right").GlobalPosition);
+					break;
+				case Checkpoint.SPAWN_LOCATIONS.FRONT:
+					player.ResetAndReposition(activeCheckpoint.GetNode<Node3D>("SpawnLocations/Front").GlobalPosition);
+					break;
+			}
+		}
+		else if (Health <= 0 && live < 2)
+		{
+			await ToSignal(GetTree().CreateTimer(1), "timeout");
+			playerDead = true;
+			EmitSignal(SignalName.PlayerDead, playerDead);
+			EmitSignal(SignalName.PlayerDied, live);
+			GetTree().Paused = true;
+			Input.MouseMode = Input.MouseModeEnum.Visible;
+		}
+	}
 
-  public void _AddLive(){
-	if(live < 2) live++;
-  }
+	public void _OnPlayerRegister(Checkpoint checkpoint)
+	{
+		activeCheckpoint.SetActive(false);
+		activeCheckpoint = checkpoint;
+	}
+
+	public void _AddLive()
+	{
+		if (live < 2) live++;
+	}
 }
