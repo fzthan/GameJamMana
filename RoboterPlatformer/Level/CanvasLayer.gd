374ea38bc6aaf88960ce7af190e435a65deb1e13
Jan Nothacker <git@jan-nothacker.de>
2024-04-02 10:51:30 +0200

    volume slider improvements

--- RoboterPlatformer/Level/CanvasLayer.gd
+++ RoboterPlatformer/Level/CanvasLayer.gd
@@ -1,13 +1,17 @@
 extends Control
 
+var master_bus_index: int;
+
+func _ready():
+  master_bus_index = AudioServer.get_bus_index("Master");
 
 func onNewGame():
-	get_tree().change_scene_to_file("res://Level/Large.tscn")
+  get_tree().change_scene_to_file("res://Level/Large.tscn")
 
 
 func onExit():
-	get_tree().quit()
+  get_tree().quit()
 
 
 func onValueChanged(value):
-	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Master"), linear_to_db(value))
+  AudioServer.set_bus_volume_db(master_bus_index, linear_to_db(value))
